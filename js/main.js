1a18058808bc1fefbfa2ae6fcf0c54f3f7c2786d
Jasur <165104527+jasurgraduate@users.noreply.github.com>
2024-04-05 22:30:11 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+const text = "Hello! Welcome to my personal website. Here you can see my personal projects and read my blogs :) ";
+
+// Function to type out text with a cursor
+function typeWriter(text, i = 0) {
+  if (i < text.length) {
+    document.getElementById('typing-text').innerHTML += text.charAt(i);
+    i++;
+    setTimeout(() => typeWriter(text, i), 50); // Adjust typing speed (milliseconds)
+  } else {
+    // Create cursor element after typing is finished
+    document.getElementById('typing-text').insertAdjacentHTML('beforeend', '<span class="cursor"></span>');
+  }
+}
+
+// Start typing effect
+typeWriter(text);
+
 
