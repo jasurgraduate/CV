838474f0598fd4f933d0e06974a76b0c23837343
Jasur <165104527+jasurgraduate@users.noreply.github.com>
2024-06-18 22:41:22 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,34 +15,3 @@ function typeWriter(text, i = 0) {
 // Start typing effect
 typeWriter(text);
 
-
-// Profile slide show
-const imageUrls = ['pic3.png', 'pic2.png', 'pic.png', 'pic1.png']; // Add more image URLs as needed
-let currentIndex = 0;
-const profilePic = document.querySelector('.profile-pic');
-let interval;
-
-// Function to change the profile picture
-function changeProfilePic() {
-    profilePic.src = imageUrls[currentIndex];
-    currentIndex = (currentIndex + 1) % imageUrls.length;
-}
-
-// Function to handle click event and reset the interval
-function handleClick() {
-    changeProfilePic();
-    resetInterval();
-}
-
-// Function to reset the interval
-function resetInterval() {
-    clearInterval(interval);
-    interval = setInterval(changeProfilePic, 4000);
-}
-
-// Add event listener for click event
-profilePic.addEventListener('click', handleClick);
-
-// Call the changeProfilePic function initially and start the interval
-changeProfilePic(); // Call once to set initial image
-resetInterval(); // Start the interval
